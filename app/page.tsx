6c3f083639c46330e453d8d60defdb196c607583
ajmalthegoat <marcmisgreat@gmail.com>
2024-06-02 07:21:53 +0100

    new repo

---
+++ app/page.tsx
@@ -0,0 +1,63 @@
+'use client';
+import Button from "./components/CustomButtonComponent";
+
+var round = 0;
+var t = 0;
+var h = 0;
+var max_participants = 10;
+var participants = max_participants;
+
+export function flip() {
+  var toss = Math.floor(Math.random() * 2) == 0;
+  if (toss){
+    return "tails"
+  } else {
+    return "heads"
+  }
+}
+
+export function calculate(){
+  t = 0;
+  h = 0;
+
+  for (var i=0; i < participants; i++) {
+    if (flip() == "tails"){
+      t += 1;
+    } else {
+      h += 1;
+    }
+  }
+  round += 1;
+  participants = t;
+
+  console.log("Tails: " + t, "Heads: " + h, "Round: " + round)
+  if (participants == 1){
+    console.log("One participant made it to the end!")
+    participants = max_participants;
+    round = 0;
+  } else if (participants <= 0){
+    console.log("None of the participants made it to the end!")
+    participants = max_participants;
+    round = 0;
+  }
+  
+}
+
+export default function Home() {
+  return (
+    
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <Button 
+          border="none"
+          color="teal"
+          height = "200px"
+          onClick={() => calculate()}
+          radius = "50%"
+          width = "200px"
+          children = "Calculate Number of Coin Flips"
+        />
+        </div>
+    </main>
+  );
+}
