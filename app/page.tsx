c50f0491f70800699519f95e785d5254dd3ec1e9
ajmalthegoat <marcmisgreat@gmail.com>
2024-06-04 05:22:30 +0100

    broken text box

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,131 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Box,
   Button,
+  ChakraProvider, 
+  Box,
   Flex,
-  IconButton,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  Stack,
-  Text,
-  useColorMode,
+  VStack,
+  HStack,
+  Text
 } from '@chakra-ui/react';
 
-var round = 0;
-var t = 0;
-var h = 0;
-var max_participants = 10;
-var participants = max_participants;
+export default function Home() {
 
-export function flip() {
-  var toss = Math.floor(Math.random() * 2) == 0;
-  if (toss){
-    return "tails"
-  } else {
-    return "heads"
-  }
-}
+  var round = 0;
+  var t = 0;
+  var h = 0;
+  var max_participants = 1000;
+  var participants = max_participants;
+  
+  var [participant_text, set_participant_text] = useState("Participants Left: " + participants) ;
+  var [tails_text, set_tails_text] = useState("Tails: " + t) ;
+  var [heads_text, set_heads_text] = useState("Heads: " + h) ;
+  var [round_text, set_rounds_text] = useState("Round: " + round) ;
 
-export function calculate(){
-  t = 0;
-  h = 0;
-
-  for (var i=0; i < participants; i++) {
-    if (flip() == "tails"){
-      t += 1;
+  const flip = () =>{
+    var toss = Math.floor(Math.random() * 2) == 0;
+    if (toss){
+      return "tails"
     } else {
-      h += 1;
+      return "heads"
     }
   }
-  round += 1;
-  participants = t;
-
-  console.log("Tails: " + t, "Heads: " + h, "Round: " + round)
-  if (participants == 1){
-    console.log("One participant made it to the end!")
-    participants = max_participants;
-    round = 0;
-  } else if (participants <= 0){
-    console.log("None of the participants made it to the end!")
-    participants = max_participants;
-    round = 0;
-  }
   
-}
+  const set_textbox = (participants: number) => {
+    set_participant_text("Participants Remaining: " + t)
+    set_tails_text("Tails: " + t)
+    set_heads_text("Heads: " + h)
+    set_rounds_text("Round: " + round)
+  }
 
-export default function Home() {
+  const calculate = () => {
+    t = 0;
+    h = 0;
+  
+    for (var i=0; i < participants; i++) {
+      if (flip() == "tails"){
+        t += 1;
+      } else {
+        h += 1;
+      }
+    }
+    round += 1;
+    participants = t;
+  
+    console.log("Tails: " + t, "Heads: " + h, "Round: " + round)
+    if (participants == 1){
+      console.log("One participant made it to the end!")
+      participants = max_participants;
+      round = 0;
+
+    } else if (participants <= 0){
+      console.log("None of the participants made it to the end!")
+      participants = max_participants;
+      round = 0;
+    }
+    //set_textbox(t);
+  }
+
   return (
     
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="fixed flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <Button 
-          border="none"
-          color="teal"
-          height = "200px"
-          onClick={() => calculate()}
-          radius = "50%"
-          width = "200px"
-          children = "Calculate Number of Coin Flips"
-        />
-        </div>
-        <div className="fixed flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <Button 
-          border="none"
-          color="teal"
-          height = "200px"
-          onClick={() => calculate()}
-          radius = "50%"
-          width = "200px"
-          children = "Calculate Number of Coin Flips"
-        />
-        <Button 
-          border="none"
-          color="teal"
-          height = "200px"
-          onClick={() => calculate()}
-          radius = "50%"
-          width = "200px"
-          children = "Calculate Number of Coin Flips"
-        />
-        </div>
+    <main className="flex min-h-screen flex-co justify-between">
+      <ChakraProvider>
+      <Box height="7vh" bg="blue.500" color="white" textAlign="center">
+        <Text fontSize="xl" fontWeight="bold" p={6} display="flex" alignItems="center" justifyContent="center">
+          The 1000 Coin Flip Experiment
+        </Text>
+      </Box>
+        <Box minH="93vh" bgGradient="linear(to-b, gray.200, gray.500)">
+          <Flex height="90vh" alignItems="center" justifyContent="center">
+          <VStack spacing={8} align="center" justify="center" height="30vh">
+            <VStack spacing={28} align="center" justify="center" height="100vh">
+                <Button
+                  p={6} 
+                  colorScheme='blue' 
+                  variant='solid'
+                  onClick={() => calculate()}>
+                  <Text 
+                    fontSize="xl" 
+                    fontWeight="bold">
+                    Calculate Number of Coin Flips
+                  </Text>
+                </Button>
+                <HStack spacing={4} align="center" justify="center" height="10vh">
+                  <Box bg="blue.500" color="white" p={4} textAlign="center" borderRadius={8}>
+                  <Text 
+                      fontSize="xl" 
+                      fontWeight="bold">
+                      {tails_text}
+                    </Text>
+                  </Box>
+                  <Box bg="blue.500" color="white" p={4} textAlign="center" borderRadius={8}>
+                  <Text 
+                      fontSize="xl" 
+                      fontWeight="bold">
+                      {heads_text}
+                    </Text>
+                  </Box>
+                </HStack>
+              </VStack>
+                  <Box bg="blue.500" color="white" p={4} textAlign="center" borderRadius={8}>
+                  <Text 
+                      fontSize="xl" 
+                      fontWeight="bold">
+                      {participant_text}
+                    </Text>
+                  </Box>
+                  <Box bg="blue.500" color="white" p={4} textAlign="center" borderRadius={8}>
+                  <Text 
+                      fontSize="xl" 
+                      fontWeight="bold">
+                      {round_text}
+                    </Text>
+                  </Box>
+              </VStack>
+          </Flex>
+        </Box>
+      </ChakraProvider>
     </main>
   );
 }
